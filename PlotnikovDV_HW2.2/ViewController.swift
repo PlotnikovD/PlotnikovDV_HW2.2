08b61f35fb753876699225c91a20b909d802ff50
D.Plotnikov <plotnikdima5296@gmail.com>
2021-10-20 21:45:20 +0500

    Initial Commit

---
+++ PlotnikovDV_HW2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlotnikovDV_HW2.2
+//
+//  Created by Dmitrii on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
