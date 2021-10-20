e3223e977c2890e98ef6f105aef30daae8167e24
D.Plotnikov <plotnikdima5296@gmail.com>
2021-10-20 22:10:20 +0500

    PlotnikovDV_HW2.2

--- PlotnikovDV_HW2.2/ViewController.swift
+++ PlotnikovDV_HW2.2/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redCircle: UIView!
+    @IBOutlet var yellowCircle: UIView!
+    @IBOutlet var greenCircle: UIView!
+    @IBOutlet var buttonText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redCircle.alpha = 0.3
+        yellowCircle.alpha = 0.3
+        greenCircle.alpha = 0.3
+        
+        redCircle.layer.cornerRadius = 80
+        yellowCircle.layer.cornerRadius = 80
+        greenCircle.layer.cornerRadius = 80
+
     }
+    
+    @IBAction func switchColorButtonPressed() {
+        buttonText.setTitle("NEXT", for: .normal)
 
-
-}
-
+        if (redCircle.alpha != 1) && (yellowCircle.alpha != 1) && (greenCircle.alpha != 1) {
+                 redCircle.alpha = 1
+             } else if redCircle.alpha == 1 {
+                 redCircle.alpha = 0.3
+                 yellowCircle.alpha = 1
+             } else if yellowCircle.alpha == 1 {
+                 yellowCircle.alpha = 0.3
+                 greenCircle.alpha = 1
+             } else if greenCircle.alpha == 1 {
+                 greenCircle.alpha = 0.3
+                 redCircle.alpha = 1
+             }
+     }
+     }
